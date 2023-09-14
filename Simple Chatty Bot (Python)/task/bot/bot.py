68a8846c2083a1e20a1f4116fe044b1f23bef82c
paweso <paweso@wp.pl>
2023-09-14 19:04:09 +0200

    Add guess the age

--- Simple Chatty Bot (Python)/task/bot/bot.py
+++ Simple Chatty Bot (Python)/task/bot/bot.py
@@ -1,4 +1,4 @@
-bot_name = "Aid"
+bot_name = 'Aid'
 birth_year = 2023
 
 print(f'Hello! My name is {bot_name}.')
@@ -8,3 +8,13 @@ print('Please, remind me your name.')
 your_name = input()
 
 print(f'What a great name you have, {your_name}!')
+print('Let me guess your age.')
+print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+remainder3 = int(input())
+remainder5 = int(input())
+remainder7 = int(input())
+
+age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+
+print(f"Your age is {age}; that's a good time to start programming!")
